defcb32d19e24aab8e27101c38c081c47cba4cf2
rinckoarmg <rinckoar@gmail.com>
2022-01-31 08:59:44 -0500

    stage 4

--- web/modules/custom/perfil/src/Controller/PerfilController.php
+++ web/modules/custom/perfil/src/Controller/PerfilController.php
@@ -6,7 +6,7 @@ class PerfilController {
     public function content() {
         return [
             '#type' => 'markup',
-            '#markup' => t('Este es el perfil de Diana Marcela'),
+            '#markup' => t('Este es el perfil de la empresa'),
         ];
     }
 }
